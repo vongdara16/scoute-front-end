61837bb1c50170ff6015b7addd37a00eb1cb1f9c
Justin Vongdara <vongdara16@gmail.com>
2022-03-24 19:05:32 -0400

    add divs to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,17 @@
 const Home = (props) => {
   return (  
-    <h1>Homies Page</h1>
+    <>
+      <h1>Homies Page</h1>
+      <div>
+        <h2>Find Restrooms</h2>
+      </div>
+      <div>
+        <h2>Find Restaurants</h2>
+      </div>
+      <div>
+        <h2>Find Parking</h2>
+      </div>
+    </>
 
 
   );
