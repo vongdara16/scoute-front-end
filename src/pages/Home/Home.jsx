07e1d6fdbd30207147aaac8312d47c3ecf19409f
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-03-25 14:34:58 -0500

    add styling to landing and home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = (props) => {
 
   return (  
     <>
-      <h1>Homies Page</h1>
-      <div className="card">
+      <div className="card" id="home-card">
         <Link to="/restrooms">
           <img 
             src="https://picsum.photos/id/398/640/480" 
@@ -13,11 +13,11 @@ const Home = (props) => {
             alt="..." 
           />
           <div className="card-body" title='restrooms'>
-            <h5 className="card-title">Find a Restroom</h5>
+            <h5 id="link" className="card-title">Find a Restroom</h5>
           </div>
         </Link>
       </div>
-      <div className="card">
+      <div className="card" id="home-card" >
         <Link to="/restaurants">
           <img 
             src="https://picsum.photos/id/676/640/480" 
@@ -25,11 +25,11 @@ const Home = (props) => {
             alt="..." 
           />
           <div className="card-body" title='restaurants'>
-            <h5 className="card-title">Find a Restaurant</h5>
+            <h5 id="link" className="card-title">Find a Restaurant</h5>
           </div>
         </Link>
       </div>
-      <div className="card">
+      <div className="card" id="home-card" >
         <Link to="/parkinglots">
           <img 
             src="https://picsum.photos/id/648/640/480" 
@@ -37,7 +37,7 @@ const Home = (props) => {
             alt="..." 
           />
           <div className="card-body" title='parkinglots'>
-            <h5 className="card-title">Find Parking</h5>
+            <h5 id="link" className="card-title">Find Parking</h5>
           </div>
         </Link>
       </div>
