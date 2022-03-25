c3ca2c3de535aae9bc590b93167379727bb48932
Justin Vongdara <vongdara16@gmail.com>
2022-03-25 16:03:20 -0400

    fix conflicts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import NavBarTop from "../../components/NavBarTop/NavBarTop";
 import './Home.css'
+import NavBarTop from "../../components/NavBarTop/NavBarTop";
 
 const Home = (props) => {
 
