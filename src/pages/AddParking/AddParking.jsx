156fa4286ad64f955a9b7d0ba4f165dd3b4a4cbf
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-03-28 16:17:16 -0500

    add street and city input forms

--- src/pages/AddParking/AddParking.jsx
+++ src/pages/AddParking/AddParking.jsx
@@ -4,7 +4,6 @@ const AddParking = (props) => {
   const formElement = useRef()
   const [validForm, setValidForm] = useState(false)
   const [formData, setFormData] = useState({
-    author: '',
     street: '',
     city: '',
     state: '',
@@ -23,11 +22,14 @@ const AddParking = (props) => {
 		setFormData({...formData, photo: evt.target.files[0]})
 	}
 
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
 
   const handleSubmit = evt => {
     evt.preventDefault()
     const parkingFormData = new FormData()
-    parkingFormData.append('author', formData.author)
     parkingFormData.append('street', formData.street)
     parkingFormData.append('city', formData.city)
     parkingFormData.append('state', formData.state)
@@ -39,7 +41,40 @@ const AddParking = (props) => {
   }
   return (
     <>
-      <div>add puppies</div>
+      <div>add parking</div>
+      <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+        <div className="form-group mb-1" >
+          <label 
+          htmlFor="stree-input" 
+          className="form-label">
+            Street
+          </label>
+          <input 
+          type="text"
+          className="form-control"
+          id="street-input"
+          name="street"
+          value={formData.street}
+          onChange={handleChange}
+          required
+        />
+        </div>
+        <div className="form-group mb-1">
+          <label 
+          htmlFor="city-input" 
+          className="form-label">
+            City
+          </label>
+          <input
+          type="text"
+          className="form-control"
+          id="city-input"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+          required/>
+        </div>
+      </form>
     </>
     );
 }
