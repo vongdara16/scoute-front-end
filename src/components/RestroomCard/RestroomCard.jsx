1d481f6b2d4e86891e84648d6ae34b257e6cb14b
Fabian Silva <silvafabian74@yahoo.com>
2022-03-30 17:25:48 -0400

    stub up initial styling for restroom list

--- src/components/RestroomCard/RestroomCard.jsx
+++ src/components/RestroomCard/RestroomCard.jsx
@@ -3,8 +3,22 @@ import { Link } from "react-router-dom";
 const RestroomCard = ({restroom}) => {
   return ( 
     <>
-    <h2>This is shitters</h2>
-    
+      <div className="card" id="home-card">
+          <img 
+            src=
+              {restroom.image_url ? restroom.image_url : restroom.photo ? restroom.photo : 'https://picsum.photos/id/398/640/480' } 
+            alt="A happy puppy"
+            className="card-img-top"
+          />
+          <div className="card-body" title='restrooms'>
+            <h5 id="link" className="card-title">
+              {restroom.name ? restroom.name : 'N/A' }
+            </h5>
+            <div id='distance' >
+              <span id='distance' >{(restroom.distance/1609.34).toFixed(2)} mi</span>
+            </div>
+          </div>
+      </div>
     </>
   );
 }
